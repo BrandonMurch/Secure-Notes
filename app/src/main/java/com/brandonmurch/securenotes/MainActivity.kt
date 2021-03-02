358805b5f5bddb07b2c83b59f166d8b0da8b2927
Brandon Murch <36788304+BrandonMurch@users.noreply.github.com>
2021-03-01 22:41:50 -0500

    Add note stub for testing

--- app/src/main/java/com/brandonmurch/securenotes/MainActivity.kt
+++ app/src/main/java/com/brandonmurch/securenotes/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.brandonmurch.securenotes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private fun getNotes() : MutableList<Note> {
+        val notes =  ArrayList<Note>()
+        notes.add(Note("Test Note", "this is a note body."))
+        notes.add(Note("Test Note 2", "electric boogaloo"))
+        return notes
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView : RecyclerView = findViewById(R.id.recyclerNotes)
+        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        recyclerView.adapter = NoteAdapter(getNotes())
     }
 }
