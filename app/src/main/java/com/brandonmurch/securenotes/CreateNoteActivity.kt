49a2aa196f6fe0a9b2191027065ecabd1dd22111
Brandon Murch <36788304+BrandonMurch@users.noreply.github.com>
2021-03-10 07:57:52 -0500

    Add create note activity and link via button

---
+++ app/src/main/java/com/brandonmurch/securenotes/CreateNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.brandonmurch.securenotes
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateNoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_note)
+    }
+}
--- app/src/main/java/com/brandonmurch/securenotes/MainActivity.kt
+++ app/src/main/java/com/brandonmurch/securenotes/MainActivity.kt
@@ -3,10 +3,12 @@ package com.brandonmurch.securenotes
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.brandonmurch.securenotes.NoteAdapter.OnNoteListener
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(), OnNoteListener {
 
@@ -26,6 +28,11 @@ class MainActivity : AppCompatActivity(), OnNoteListener {
         val recyclerView : RecyclerView = findViewById(R.id.recyclerNotes)
         recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         recyclerView.adapter = NoteAdapter(notes, this)
+
+        val createNoteButton : FloatingActionButton = findViewById(R.id.floatingButtonNewNote)
+        createNoteButton.setOnClickListener {
+            startActivity(Intent(this, CreateNoteActivity::class.java))
+        }
     }
 
     override fun onNoteClick(position: Int) {
